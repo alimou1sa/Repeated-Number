3a93b8557e8f3deb616e0c3b5c4a7b1b9576c2ff
Dev.Ali Mousa <ali.mou1sa@gmail.com>
2024-10-05 12:02:33 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Project_10_Repeated_number
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(49, 76);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(605, 499);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tempus Sans ITC", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(605, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 61);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "__";
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BorderRadius = 33;
+            this.guna2Button1.BorderThickness = 3;
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.FillColor = System.Drawing.Color.Purple;
+            this.guna2Button1.Font = new System.Drawing.Font("Showcard Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Location = new System.Drawing.Point(762, 158);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(109, 88);
+            this.guna2Button1.TabIndex = 3;
+            this.guna2Button1.Text = "Check";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2TextBox1
+            // 
+            this.guna2TextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox1.DefaultText = "";
+            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.DisabledState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.FocusedState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.HoverState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.Location = new System.Drawing.Point(1254, 767);
+            this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
+            this.guna2TextBox1.Name = "guna2TextBox1";
+            this.guna2TextBox1.PasswordChar = '\0';
+            this.guna2TextBox1.PlaceholderText = "";
+            this.guna2TextBox1.SelectedText = "";
+            this.guna2TextBox1.ShadowDecoration.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.Size = new System.Drawing.Size(433, 85);
+            this.guna2TextBox1.TabIndex = 4;
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.BorderRadius = 33;
+            this.guna2Button2.BorderThickness = 3;
+            this.guna2Button2.CheckedState.Parent = this.guna2Button2;
+            this.guna2Button2.CustomImages.Parent = this.guna2Button2;
+            this.guna2Button2.FillColor = System.Drawing.Color.Purple;
+            this.guna2Button2.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.HoverState.Parent = this.guna2Button2;
+            this.guna2Button2.Location = new System.Drawing.Point(762, 32);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.ShadowDecoration.Parent = this.guna2Button2;
+            this.guna2Button2.Size = new System.Drawing.Size(109, 88);
+            this.guna2Button2.TabIndex = 5;
+            this.guna2Button2.Text = "Set";
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Blue;
+            this.label2.Font = new System.Drawing.Font("Viner Hand ITC", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(146, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(388, 69);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Repeated Number";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.DarkViolet;
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(638, 392);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 40);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(894, 619);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.guna2TextBox1);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,187 @@
+﻿using Guna.UI2.WinForms.Suite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_10_Repeated_number
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        
+        }
+
+        Random rdb = new Random();
+        bool x = false;
+        byte number;
+        byte counter;
+
+
+        Label creatbutton(short X,short Y)
+        {
+    
+
+            Label lbl1 = new Label();
+          
+            lbl1.ForeColor = Color.Green;
+            lbl1.BackColor = Color.Black;
+            lbl1.Size = new System.Drawing.Size(80, 70);
+            lbl1.Font = new Font("Tahoma", 16, FontStyle.Regular);
+            lbl1.Text = 0.ToString();
+            lbl1.TextAlign = ContentAlignment.MiddleCenter;
+            lbl1.BorderStyle = BorderStyle.FixedSingle;
+            lbl1.Location = new System.Drawing.Point(X, Y);
+
+
+            
+            panel1.Controls.Add(lbl1);
+            return lbl1;
+
+        }
+
+        void Addnumbers()
+        {
+            if(x==true)
+            {
+                panel1.Controls.Clear();
+                x = false;
+            }
+
+            counter = 0;
+            short X = 40;
+            short Y = 10;
+            Label labl;
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    labl = creatbutton(X,Y);
+
+                    number=Convert.ToByte(rdb.Next(1, 10).ToString());
+                    labl.Text = number.ToString();
+                    labl.Tag = number;
+                    panel1.Controls.Add(labl);
+
+                    if (label1.Text == labl.Tag.ToString())
+                    {
+                        counter++;
+                    }
+
+                    X += 80;
+                }
+
+                X = 40;
+                Y += 70;
+            }
+
+            x = true;
+
+        }
+
+        void CheckAnswer()
+        {
+            if (textBox1.Text==counter.ToString())
+            {
+                label1.Text = "correct";
+            }
+            else
+            {
+                label1.Text = "Wrong";
+            }
+
+        }
+
+        void Main()
+        {
+            // Specify the path to your .wav file  
+            string path = @"C:\Users\TOPTECH\Music\Shot.wav";
+
+            // Create a SoundPlayer instance  
+            SoundPlayer player = new SoundPlayer(path);
+
+         
+                // Load the .wav file  
+                player.Load();
+                player.Play();
+                // Play the song  
+                //player.PlaySync(); // Use Play() for asynchronous play  
+
+               // Console.WriteLine("Song is playing...");
+   
+
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+
+     
+
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                textBox1.Focus();
+                MessageBox.Show("Text Box should have a value!", "Wrong");
+
+            }
+            else
+            {
+                Main();
+                CheckAnswer();
+
+            }
+
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+
+            //if (string.IsNullOrWhiteSpace(textBox1.Text))
+            //{
+            //    textBox1.Focus();
+            //    MessageBox.Show( "Text Box should have a value!", "Wrong");
+
+            //}
+            //else
+            //{
+            number = Convert.ToByte(rdb.Next(1, 10));
+            label1.Text = number.ToString();
+            Addnumbers();
+
+            // }
+        }
+
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+
+            ErrorProvider er = new ErrorProvider();
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+
+               e.Cancel = true;
+               textBox1.Focus();
+               er.SetError(textBox1, "should have a value!");
+            }
+            else
+            {
+                e.Cancel = false;
+                er.SetError(textBox1, "");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+        }
+    }
+}
